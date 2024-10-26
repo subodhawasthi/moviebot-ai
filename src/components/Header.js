562ee00c32b2a6d50f8eb6cdb04c9c92b56e4b0f
Subodh Kant Awasthi <subodhawasthi@Subodhs-MacBook-Air.local>
2024-10-26 14:10:43 +0530

    Fetched TMDB data & built Main Container

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,7 @@ const Header = () => {
       });
   };
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         const { uid, email, displayName } = user;
         dispatch(addUser({ uid: uid, email: email, displayName: displayName }));
@@ -32,6 +32,7 @@ const Header = () => {
         navigate("/");
       }
     });
+    return () => unsubscribe();
   }, []);
   return (
     <div className="absolute px-8 py-2 w-screen bg-gradient-to-b from-black z-10 flex justify-between">
---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNTMyNDhmNmQ3YWZlNTBlZTI5ODczN2ZlOTRiMWMyZSIsIm5iZiI6MTcyOTg3NjY0OS41MDE5NDIsInN1YiI6IjY3MWJjZmZmYTRhYzhhNDMyYzVjNDgyMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.30YoYARYkzQgQ5p1pNPoTTzKb_ZqVtxRuB1KvxorLq8",
+  },
+};
